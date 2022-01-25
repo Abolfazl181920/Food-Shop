c7c145ebce3758e17fdf5fad2b116e193ee48255
Abolfazl <abolfazlm181920@gmail.com>
2022-01-25 23:05:13 +0330

    add liked item

--- src/components/Panel/FoodsApi/ShowFoods.js
+++ src/components/Panel/FoodsApi/ShowFoods.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const ShowFoods = ({ title, image }) => {
+import { HeartOutline } from 'react-ionicons';
+
+const ShowFoods = ({ title, image, key }) => {
+
+    const [ likedColor, setLikedColor ] = useState('#FFFFFF');
+    const [ liked, setLiked ] = useState(true);
+
+    const handleLikedItem = () => {
+        setLiked(!liked);
+        setLikedColor('#FF0000');
+    }
+
     return (
         <div className="ShowFoodBody rounded-2xl ml-4">
             <div className="w-64 h-72">
@@ -9,8 +20,16 @@ const ShowFoods = ({ title, image }) => {
                     <img src={image} className="w-32 cursor-pointer rounded-full ml-14 border-4 -mt-8" />
                 </Link>
                 <h2 className="text-white text-center mt-8 text-xl"> {title} </h2>
-                {/* <span>Price: 2500$</span> */}
                 <span className="text-indigo-500 animate-pulse ml-24 -mt-32">15% Off</span>
+
+                    <HeartOutline
+                        key={key}
+                        onClick={handleLikedItem}
+                        style={{ marginLeft: '200px', marginTop: '30px', cursor: 'pointer' }}
+                        color={likedColor}
+                        height="27px"
+                        width="27px"
+                    />
             </div>
         </div>
     );
